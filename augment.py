d60b82db13381490adcdb76b2534284a6be93d0f
ReeseHatfield <hatfield.69@wright.edu>
2024-09-05 19:52:04 -0400

    semi working augmentor

--- augment.py
+++ augment.py
@@ -1,10 +1,14 @@
 from tensorflow import keras
 from keras._tf_keras.keras.preprocessing.image import ImageDataGenerator
+from keras.utils import array_to_img, img_to_array, load_img
+from keras._tf_keras.keras.utils import array_to_img, img_to_array, load_img
+
 import sys
 import os
 
 def main():
     
+    # path already resolved
     dir_name = sys.argv[1]
     print(dir_name)
 
@@ -18,11 +22,26 @@ def main():
     
     files = os.listdir(dir_name)
     
-    
     # only use image fields
     files = list(filter(lambda s: s.endswith((".jpg", ".jpeg", ".png")), files))
     
     
+    
+    for file in files:
+        cur_img = load_img(os.path.join(dir_name, file))
+        
+        data_arr = img_to_array(cur_img)
+        
+        # wtf is this?
+        data_arr = data_arr.reshape((1, ) + data_arr.shape) 
+        
+        i = 0
+        for batch in datagen.flow(data_arr, batch_size = 1,
+                                save_to_dir ='out', 
+                                save_prefix ='image', save_format ='jpeg'):
+            i += 1
+            if i > 10:
+                break
     
 
 
